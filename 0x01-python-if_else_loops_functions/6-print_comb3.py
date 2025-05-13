e0969a8ecdc30cbd292af914453125478331f447
marwan-99 <marwanboudraforever2199@gmail.com>
2025-05-13 16:07:55 -0700

    combination of brains

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first_digit in range(0, 10):
     for second_digit in range(first_digit + 1, 10):
         if first_digit < 8 and second_digit < 10:
-            print("{}{}".format(first_digit, second_digit), end=', ')
+            print("{}{}, ".format(first_digit, second_digit), end='')
         elif first_digit == 8 and second_digit == 9:
-            print("{}{}".format(first_digit, second_digit), end=' ')
+            print("{}{}".format(first_digit, second_digit), end='')
